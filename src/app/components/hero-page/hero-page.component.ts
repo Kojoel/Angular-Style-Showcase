546eda57d8e65d261b386d27b56481c446544a93
Kojoel <nanaabruquah1s@gmail.com>
2024-09-11 16:59:23 +0000

    hero page UI update

--- src/app/components/hero-page/hero-page.component.ts
+++ src/app/components/hero-page/hero-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HeroService } from '../../services/hero.service';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { Observable, interval, map, of, switchMap } from 'rxjs';
+import { Hero } from '../../model/hero.model';
 
 @Component({
   selector: 'app-hero-page',
@@ -11,6 +13,32 @@ import { AsyncPipe, CommonModule } from '@angular/common';
 })
 export class HeroPageComponent {
 
+  itemCount: number = 0;
+
+  heroItems$: Observable<Hero[]> | undefined;
+
   constructor(public heroService: HeroService) {}
 
+  ngOnInit() {
+    this.heroService.getHeroes().subscribe(item => this.heroItems$ = of(item))
+  }
+
+  leftBtn() {
+    if(this.itemCount <= 0) {
+      this.itemCount = 4;
+    }
+    else{
+      this.itemCount = this.itemCount - 1;
+    }
+  }
+
+
+  rightBtn() {
+    if(this.itemCount >= 4) {
+      this.itemCount = 0;
+    }
+    else{
+      this.itemCount = this.itemCount + 1;
+    }
+  }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../model/hero.model';
-import { Observable } from 'rxjs';
+import { Observable, interval, map, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +12,18 @@ export class HeroService {
 
   constructor(private http: HttpClient) { }
 
-  getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroDataUrl);
+  getHeroes() {
+    return this.http.get<Hero[]>(this.heroDataUrl)
   }
+  // getHeroes(): Observable<Hero> {
+  //   return this.http.get<Hero[]>(this.heroDataUrl).pipe(
+  //     switchMap((heroes) => {
+  //       return interval(5000).pipe(
+  //         map((index) => heroes[index % heroes.length]) // Emits one hero every 5 seconds
+  //       );
+  //     })
+  //   );
+  // }
 
 
 }
