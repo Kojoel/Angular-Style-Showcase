23f1f0661c36c29d214cdbda0a3ab9efe54a934a
Kojoel <nanaabruquah1s@gmail.com>
2024-09-12 08:31:04 +0000

    Update to theme

--- src/app/components/reasons/reasons.component.ts
+++ src/app/components/reasons/reasons.component.ts
@@ -3,6 +3,7 @@ import { ReasonService } from '../../services/reason.service';
 import { Observable, of } from 'rxjs';
 import { Reason } from '../../model/reason.model';
 import { AsyncPipe } from '@angular/common';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-reasons',
@@ -15,7 +16,7 @@ export class ReasonsComponent {
 
   reasonItems$: Observable<Reason[]> | undefined;
 
-  constructor(public reasons: ReasonService) {}
+  constructor(public reasons: ReasonService, public theme: ThemeService) {}
 
   ngOnInit() {
     this.reasons.getReasons().subscribe(item => this.reasonItems$ = of(item))
